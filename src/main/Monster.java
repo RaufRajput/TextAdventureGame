376194b03cb78a25dd2eb06e55fdedcba8c28c05
Rikard <Rickard.forslund@live.se>
2019-12-03 22:33:18 +0100

    Monster Class Added

---
+++ src/main/Monster.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Monster implements CreatureManager {
+
+    @Override
+    public void Health(int a) {
+
+    }
+
+    @Override
+    public void gender(Gender gender) {
+
+    }
+
+    @Override
+    public void Race(Race race) {
+
+    }
+}
