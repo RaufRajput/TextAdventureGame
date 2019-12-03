1919c07941a7d1a89cd078399bddac8248f11b69
Rikard <Rickard.forslund@live.se>
2019-12-03 22:47:40 +0100

    Enum Tests Added

---
+++ src/test/main/RaceTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class RaceTest {
+
+    @Test
+    void TestRaceEnums() {
+        assertEquals("HUMAN", Race.HUMAN.toString());
+        assertEquals("ELF", Race.ELF.toString());
+        assertEquals("OGRE", Race.OGRE.toString());
+        assertEquals("TROLL", Race.TROLL.toString());
+    }
+}
