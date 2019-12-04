8be1075a6fbcace9ddb222f4f1ebfab6df39f5f6
Rikard <Rickard.forslund@live.se>
2019-12-04 09:11:00 +0100

    Created Player class

---
+++ src/main/Player.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Player implements CreatureManager {
+    @Override
+    public void Health(int a) {
+
+    }
+
+    @Override
+    public void gender(Gender gender) {
+
+    }
+
+    @Override
+    public void Race(Race race) {
+
+    }
+}
