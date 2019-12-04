7da88c8fb533dfecf5130b1c4f4aa32654990b99
Rauf Rajput <r.rajput@hotmail.com>
2019-12-04 11:47:55 +0100

    Updated Game, implements Fight

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,18 @@
-public class Game {
+public class Game implements Fight {
 
+
+    @Override
+    public boolean fight() {
+        return false;
+    }
+
+    @Override
+    public int attack() {
+        return 0;
+    }
+
+    @Override
+    public int defense() {
+        return 0;
+    }
 }
