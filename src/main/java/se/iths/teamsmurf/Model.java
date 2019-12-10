ba51ac07ec9f65d108a4b678322b7a745ece0eff
tenel <ten.elena@outlook.com>
2019-12-10 12:55:00 +0100

    added scenlist & monsterapperance

--- src/main/java/se/iths/teamsmurf/Model.java
+++ src/main/java/se/iths/teamsmurf/Model.java
@@ -12,7 +12,7 @@ public class Model {
     private SimpleIntegerProperty playerHealth = new  SimpleIntegerProperty();
     private SimpleIntegerProperty monsterHealth = new  SimpleIntegerProperty();
     private List<Monster> monsterList =new ArrayList<>();
-    private List<String> monsterAppearneceList = new ArrayList<>();
+    private List<String> monsterAppearanceList = new ArrayList<>();
     private List<String> sceneList = new ArrayList<>();
 
     private Model(){
@@ -20,6 +20,14 @@ public class Model {
         monsterList.add(new Monster(60,Race.TROLL));
         monsterList.add(new Monster(80,Race.OGRE));
         monsterList.add(new Monster(100,Race.HUMAN));
+        monsterAppearanceList.add(" is jumping towards you!");
+        monsterAppearanceList.add(" is popping up in front of you!");
+        monsterAppearanceList.add(" is threatening running towards you!");
+        monsterAppearanceList.add(" is moving furiously to you!");
+        sceneList.add("You are eating a taco with your friends in your favorite smurvBar when you hear a big boom outside the gate. Woooooh!!");
+        sceneList.add("Sunny Saturday! You are right in the middle of annual flower festival by the lake. Some screams are reaching you from the crowd..");
+        sceneList.add("You are in the smurvMall to pick up some groceries when you hear the ceiling crashing..");
+        sceneList.add("You are at the Christmas party with your fellows. Time for dessert. But: it´s missing! Christmas tree suddenly falls down..");
    }
     public static Model getInstance() {
         // If there is no instance available, create new one (i.e. lazy initialization).
@@ -44,8 +52,8 @@ public class Model {
     public Monster getMonster(int i){
         return monsterList.get(i);
     }
-    public String getMonsterAppearnce(int i){
-        return monsterAppearneceList.get(i);
+    public String getMonsterAppearance(int i){
+        return monsterAppearanceList.get(i);
     }
     public String getScene(int i){
         return sceneList.get(i);
