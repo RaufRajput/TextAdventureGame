ebfce7ad71cf2bd16857c6bfe65061b58fd3361e
Rikard <Rickard.forslund@live.se>
2019-12-11 10:48:48 +0100

    removed setter in Item

--- src/main/java/se/iths/teamsmurf/Item.java
+++ src/main/java/se/iths/teamsmurf/Item.java
@@ -15,23 +15,12 @@ public class Item {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getDamage() {
         return damage;
-    }
-
-    public void setDamage(int damage) {
-        this.damage = damage;
     }
 
     public int getHealth() {
         return health;
     }
 
-    public void setHealth(int health) {
-        this.health = health;
-    }
 }
